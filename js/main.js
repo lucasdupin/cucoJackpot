b3ce6541ff80284021eb35e6751f8e7e914e1828
Lucas Dupin <lucas@commit.to>
2013-05-06 23:04:57 -0300

    firefox compatibility, performance improvement

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ $(init);
 function init(){
 
 	var imagesPos = [0, 0, 0];
+	var backgroundPosX = [0, 246, 490];
 
 	setInterval(moveTheImages, 4000);
 	function moveTheImages(){
@@ -11,7 +12,7 @@ function init(){
 		for (var i = 0; i < 3; i++) {
 			imagesPos[i] += 245.333 * (parseInt(Math.random()*4)+2)
 			$('#item'+(i+1)).css({
-				backgroundPositionY: imagesPos[i]
+				backgroundPosition: backgroundPosX[i] + "px " + parseInt(imagesPos[i]) + "px"
 			});
 		};
 		
